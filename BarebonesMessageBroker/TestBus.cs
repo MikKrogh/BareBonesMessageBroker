9e77052e3731c2453667a93d53a0db9dde2d25f6
Mikael Vestergaard Krogh <mikkro89@outlook.dk>
2025-07-10 11:52:04 +0200

    added TestBus for asserting that events were published

---
+++ BarebonesMessageBroker/TestBus.cs
@@ -0,0 +1,41 @@
+﻿
+
+
+namespace BarebonesMessageBroker;
+
+public class TestBus : IBus
+{
+    private readonly List<PublishedEvent> _publishedEvents = new();
+
+    public Task Publish(object message, string eventType)
+    {
+        _publishedEvents.Add(new PublishedEvent
+        {
+            EventName = eventType,
+            Message = message
+        });
+
+        return Task.CompletedTask;
+    }
+
+    public IReadOnlyList<PublishedEvent> PublishedEvents => _publishedEvents.AsReadOnly();
+
+    public class PublishedEvent : Event
+    {
+        public object Message { get; set; }
+
+        public string Id { get; init; }
+
+        public string EventName { get; init; }
+        public DateTime Timestamp => throw new NotImplementedException();
+    }
+
+    public void Clear() => _publishedEvents.Clear();
+    public bool WasPublished<T>(string eventName, Func<T, bool>? predicate = null)
+    {
+        return _publishedEvents
+            .Where(e => e.EventName == eventName && e.Message is T)
+            .Cast<PublishedEvent>()
+            .Any(e => predicate == null || predicate((T)e.Message));
+    }
+}
